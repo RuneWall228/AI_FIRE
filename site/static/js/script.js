2d60a6c98c79501e7630bc5e096ce95f5d60055f
RuneWall228 <124618483+RuneWall228@users.noreply.github.com>
2024-12-17 20:29:24 +0300

    Delete site directory

--- site/static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('start-btn').addEventListener('click', () => {
-    document.getElementById('video-feed').src = "/video_feed";
-});
-
-document.getElementById('stop-btn').addEventListener('click', async () => {
-    document.getElementById('video-feed').src = "";
-    await fetch('/stop_camera', { method: 'POST' });
-});
